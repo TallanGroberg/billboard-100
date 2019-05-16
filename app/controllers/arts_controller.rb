dfdb86648a48daccf668d9d3a2ec2ba8c24cc464
tallan groberg <tallan.taven@gmail.com>
2019-05-16 15:12:30 -0600

    basic routes

---
+++ app/controllers/arts_controller.rb
@@ -0,0 +1,7 @@
+class ArtsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/arts_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class ArtsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get arts_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get arts_show_url
+    assert_response :success
+  end
+
+end
