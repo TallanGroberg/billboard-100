11087d9b524a6f1cad119858882613bbc4cce4c7
tallan groberg <tallan.taven@gmail.com>
2019-05-17 10:03:10 -0600

    save

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,54 @@
 class SongsController < ApplicationController
+    before_action :set_art
+    before_action :set_song, only: [:show, :edit, :update, :destroy]
+    # before_action set_bd:
+
+
   def index
+    @songs = @art.songs
+    
+
   end
 
   def show
   end
+
+  def new
+    @song = @art.songs.new
+    render partial: 'form'
+  end
+  def create
+    @song = @art.songs.new(song_params)
+    if @song.save
+      redirect_to art_songs_path
+    else
+      render partial: 'form'
+    end
+  end
+    def edit
+      render partial: 'form'
+    end
+    def update
+      if @song.update(song_params)
+        redirect_to art_song_path(@art, @song)
+      else
+        render partial: 'form'
+    end
+  end
+
+
+  private
+  def set_art
+    @art = Art.find(params[:art_id])
+  end
+
+  def set_song
+    @song = @art.songs.find(params[:id])
+     # @song = @bd.@song.find(params[:id])
+  end
+  def song_params
+    params.require(:song).permit(:name)
+  end
+  # def set_bd
+  #   @bd = Billboard.find(params[:id])
 end
